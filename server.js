e1d29d6cbea210d4462acc6390562de3e2ca9ff0
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-16 19:57:54 +0100

    async fs.readFile

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+import path from "node:path";
 import http from "node:http";
+import fs from 'node:fs/promises'
 
 const PORT = 8000;
+
+const __dirname = import.meta.dirname;
+
+const pathToResource = path.join(__dirname, 'public', 'index.html');
+
+const content = await fs.readFile(pathToResource, 'utf8');
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.statusCode = 200;
-  res.end("<html><h1>The server is working</h1></html>");
+  res.end(content);
 });
 
-server.listen(PORT, () => console.log("Hey"));
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
