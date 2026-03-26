f747f9fc657a847b0e594208dbcb6634ab71a468
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-26 12:14:33 +0100

    frontend serve error handling

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -18,6 +18,17 @@ export async function serveStatic(req, res, baseDir) {
 		const content = await fs.readFile(filePath);
 		sendResponse(res, 200, contentType, content);
 	} catch (err) {
+		if (err.code === "ENOENT") {
+			const content = await fs.readFile(path.join(publicDir, "404.html"));
+			sendResponse(res, 404, "text/html", content);
+		} else {
+			sendResponse(
+				res,
+				500,
+				"text/html",
+				"<html><h1>Server Error: ${err.code}</h1></html>",
+			);
+		}
 		console.log(err);
 	}
 }
