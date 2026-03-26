1d0032717020d79fa87f9426bc9dd3f71fe3cdef
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-26 12:12:30 +0100

    frontend serve

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,14 +1,23 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 import { sendResponse } from "./sendResponse.js";
+import { getContentType } from "./getContentType.js";
 
 export async function serveStatic(req, res, baseDir) {
-  const filePath = path.join(baseDir, "public", "index.html");
+	const publicDir = path.join(baseDir, "public");
+	const filePath = path.join(
+		publicDir,
+		req.url === "/" ? "index.html" : req.url,
+	);
 
-  try {
-    const content = await fs.readFile(filePath);
-    sendResponse(res, 200, "text/html", content);
-  } catch (err) {
-    console.log(err);
-  }
+	const ext = path.extname(filePath);
+
+	const contentType = getContentType(ext);
+
+	try {
+		const content = await fs.readFile(filePath);
+		sendResponse(res, 200, contentType, content);
+	} catch (err) {
+		console.log(err);
+	}
 }
